84f69b43b0c1b361fc02824905c40e01f8fa2f93
harsha-1807 <harshavardhan.sr@kalvium.community>
2024-02-23 10:48:16 +0530

    delete and update functionality added to website

--- Backend/Routes/routes.js
+++ Backend/Routes/routes.js
@@ -13,12 +13,31 @@ require("dotenv").config()
 //     .catch(err => console.error('Error connecting to local MongoDB:', err));
 
 //read
+
+app.use(express.json());
 app.get('/',async (req,res)=>{
     const data = await userdata.find({})
 
     res.json({success : true , data : data})
 
 })
+// get data by id 
+app.get('/:id', async (req, res) => {
+    try {
+        const id = req.params.id; // Extracting user ID from the request parameters
+        const data = await userdata.findById(id); // Finding the user by ID
+        
+        if (!data) {
+            return res.status(404).json({ success: false, message: "User not found" });
+        }
+        
+        res.json({ success: true, data: data });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ success: false, message: "Internal Server Error" });
+    }
+});
+
 
 // create data 
 app.post("/create",async (req,res)=>{
@@ -28,15 +47,32 @@ app.post("/create",async (req,res)=>{
     res.send({success : true , message : "data saved successfully"})
 })
 
+// update data by id 
+app.put("/update/:id", async (req, res) => {
+    const id = req.params.id; // Corrected from req.params._id to req.params.id
+    const newData = req.body
+    console.log(newData)
 
-// update data 
-app.put("/update/:username",async (req,res)=>{
-    let newData = req.body.UserName;
-    let username = req.params.username;
-    const data = await userdata.findOneAndUpdate({ UserName: username }, { UserName: newData })
-    res.send({success : true , message : "data updated successfully"})
+    try {
+        const data = await userdata.findOneAndUpdate({ _id: id }, newData, { new: true });
 
-})
+        if (!data) {
+            return res.status(404).send({ success: false, message: "Data not found" });
+        }
+        res.send({ success: true, message: "Data updated successfully", data: data });
+    } catch (error) {
+        console.error(error);
+        res.status(500).send({ success: false, message: "Internal Server Error" });
+    }
+});
+
+// // update data by username to change username
+// app.put("/update/:username",async (req,res)=>{
+//     let newData = req.body.UserName;
+//     let username = req.params.username;
+//     const data = await userdata.findOneAndUpdate({ UserName: username }, { UserName: newData })
+//     res.send({success : true , message : "data updated successfully"})
+// })
 
 //delete data
 app.delete("/delete/:id",async (req,res)=>{
